66f0e0bf0e552ec4959a806fb8ccabaf2a2a5a38
juliomor <juliomoraleslachica@gmail.com>
2021-11-15 16:08:53 +0000

    Laboratorio - Herramientas de Depuracion

--- Invoices/sapui5/webapp/controller/HelloPanel.controller.js
+++ Invoices/sapui5/webapp/controller/HelloPanel.controller.js
@@ -1,18 +1,30 @@
 // @ts-nocheck
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/base/Log"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller 
      * @param {typeof sap.m.MessageToast} MessageToast 
+     * @param {type of sap.base.Log} Log
      */
-    function (Controller, MessageToast) {
+    function (Controller, MessageToast, Log) {
         "use strict";
 
         return Controller.extend("alight.sapui5.controller.HelloPanel", {
 
             onInit: function () {
+            },
+
+            onBeforeRendering: function () {
+                /*window.message = "Log Message - onBeforeRendering";
+                Log.info(window.message);
+                Log.error(window.message);*/
+            },
+
+            onAfterRendering: function () {
+                //debugger;
             },
 
             onShowHello: function () {
